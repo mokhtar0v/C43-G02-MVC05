a5d319400b84227b1e1bdf7d87456c03f5a1416a
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2025-04-13 22:39:19 +0200

    Add project files.

---
+++ Demo.BusinessLogic/Services/DepartmentService.cs
@@ -0,0 +1,19 @@
+﻿using Demo.DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.BusinessLogic.Services
+{
+    internal class DepartmentService
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+
+        public DepartmentService(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+    }
+}
