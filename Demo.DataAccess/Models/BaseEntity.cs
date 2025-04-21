d6a6bba4953ae30254574d85aa0487c70769d1e8
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2025-04-21 21:53:31 +0200

    Migration

--- Demo.DataAccess/Models/BaseEntity.cs
+++ Demo.DataAccess/Models/BaseEntity.cs
@@ -12,7 +12,7 @@ namespace Demo.DataAccess.Models
         public int CreatedBy { get; set; }
         public DateTime? CreatedOn { get; set; }
         public int LastModifiedBy { get; set; }
-        public DateTime? LastModifiedOn { get; set; }
+        public DateTime LastModifiedOn { get; set; }
         public bool IsDeleted { get; set; } //soft delete
     }
 }
